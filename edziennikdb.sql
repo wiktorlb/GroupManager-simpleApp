061762a2f631e94bbdbc8135a324c918b6f5b52a
wiktorlb <82231639+wiktorlb@users.noreply.github.com>
2024-05-21 22:29:55 +0200

    Add files via upload

---
+++ edziennikdb.sql
@@ -0,0 +1,221 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 21, 2024 at 10:29 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `edziennikdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `group`
+--
+
+CREATE TABLE `group` (
+  `id_group` int(11) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `student_quantity` int(11) NOT NULL DEFAULT 0,
+  `id_teacher` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `group`
+--
+
+INSERT INTO `group` (`id_group`, `name`, `student_quantity`, `id_teacher`) VALUES
+(1, '1A', 15, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `request`
+--
+
+CREATE TABLE `request` (
+  `id_request` int(11) NOT NULL,
+  `title` varchar(128) NOT NULL,
+  `created_by` varchar(45) NOT NULL,
+  `created_at` varchar(45) NOT NULL,
+  `sent_to` varchar(45) NOT NULL,
+  `seen` tinyint(4) NOT NULL DEFAULT 0,
+  `accepted` tinyint(4) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `role`
+--
+
+CREATE TABLE `role` (
+  `id_role` int(11) NOT NULL,
+  `name` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int(11) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `last_name` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `created_at` date NOT NULL,
+  `updated_at` date DEFAULT NULL,
+  `updated_by` date DEFAULT NULL,
+  `active` tinyint(4) DEFAULT NULL,
+  `user_id_user` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id_user`, `name`, `last_name`, `password`, `email`, `created_at`, `updated_at`, `updated_by`, `active`, `user_id_user`) VALUES
+(1, 'user', 'one', 'user', 'useronegmail', '2024-05-20', NULL, NULL, 1, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_has_group`
+--
+
+CREATE TABLE `user_has_group` (
+  `User_id_user` int(11) NOT NULL,
+  `Group_id_group` int(11) NOT NULL,
+  `added_at` date NOT NULL,
+  `removed_at` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_has_role`
+--
+
+CREATE TABLE `user_has_role` (
+  `User_id_user` int(11) NOT NULL,
+  `Role_id_role` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `group`
+--
+ALTER TABLE `group`
+  ADD PRIMARY KEY (`id_group`);
+
+--
+-- Indexes for table `request`
+--
+ALTER TABLE `request`
+  ADD PRIMARY KEY (`id_request`);
+
+--
+-- Indexes for table `role`
+--
+ALTER TABLE `role`
+  ADD PRIMARY KEY (`id_role`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`),
+  ADD KEY `aaa` (`user_id_user`);
+
+--
+-- Indexes for table `user_has_group`
+--
+ALTER TABLE `user_has_group`
+  ADD PRIMARY KEY (`User_id_user`,`Group_id_group`),
+  ADD KEY `fk_User_has_Group_Group1_idx` (`Group_id_group`),
+  ADD KEY `fk_User_has_Group_User1_idx` (`User_id_user`);
+
+--
+-- Indexes for table `user_has_role`
+--
+ALTER TABLE `user_has_role`
+  ADD PRIMARY KEY (`User_id_user`,`Role_id_role`),
+  ADD KEY `fk_User_has_Role_Role1_idx` (`Role_id_role`),
+  ADD KEY `fk_User_has_Role_User1_idx` (`User_id_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `group`
+--
+ALTER TABLE `group`
+  MODIFY `id_group` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `request`
+--
+ALTER TABLE `request`
+  MODIFY `id_request` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `role`
+--
+ALTER TABLE `role`
+  MODIFY `id_role` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `aaa` FOREIGN KEY (`user_id_user`) REFERENCES `user` (`id_user`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `user_has_group`
+--
+ALTER TABLE `user_has_group`
+  ADD CONSTRAINT `fk_User_has_Group_Group1` FOREIGN KEY (`Group_id_group`) REFERENCES `group` (`id_group`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_User_has_Group_User1` FOREIGN KEY (`User_id_user`) REFERENCES `user` (`id_user`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `user_has_role`
+--
+ALTER TABLE `user_has_role`
+  ADD CONSTRAINT `fk_User_has_Role_Role1` FOREIGN KEY (`Role_id_role`) REFERENCES `role` (`id_role`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_User_has_Role_User1` FOREIGN KEY (`User_id_user`) REFERENCES `user` (`id_user`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
